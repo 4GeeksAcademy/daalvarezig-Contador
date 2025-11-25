fff3d983d541a301239389ae401f6c736de92c28
David Alvarez Iglesias <daalvarezig@gmail.com>
2025-11-25 17:18:50 +0000

    añadido reseteo automático

--- src/js/main.jsx
+++ src/js/main.jsx
@@ -15,10 +15,14 @@ let timer = 0
 const root = ReactDOM.createRoot(document.getElementById('root'))
 //forzando el renderizado cada un segundo
 setInterval(() => {
+
+// reseteo al llegar a 999999
+  if (timer > 999999) timer = 0;
+
   root.render(
     <React.StrictMode>
       <Home timer={timer} />
     </React.StrictMode>,
   )
   timer++
-}, 1000) //el setInerval crea un intervalo de tiempo (en milisegundos) donde loq ue este en el bloque de codigo, se va a repetir
+}, 1000) //el setInerval crea un intervalo de tiempo (en milisegundos) donde lo que este en el bloque de codigo, se va a repetir
